63bec74bc468413cc16881c8e831eba4f9a661a8
f1684039 <33796263+f1684039@users.noreply.github.com>
2017-12-14 12:00:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/rilak/count/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.rilak.count;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        count=0;
+    }
+
+    public void  add(View v){
+        count = count +1;
+
+        textView.setText(count+"回ボタンが押されました！");
+    }
+    public void minus(View v){
+        count =count - 1;
+        textView.setText(count+"回ボタンが押されました！");
+    }
+    public  void reset(View v){
+        count = 0;
+        textView.setText(count+"回ボタンが押されました！");
+    }
+}
